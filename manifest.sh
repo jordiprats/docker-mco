b073426337e3d6bce0841904a568b36ddffbf662
Jordi Prats <jprats@systemadmin.es>
2015-12-23 17:32:27 +0100

    liada parda

--- manifest.sh
+++ manifest.sh
@@ -5,8 +5,8 @@ class { 'mcollective':
   password => '${EYP_ACTIVEMQ_ADMIN_PASSWORD-Y2F0YWx1bnlhbGxpdXJlCg}',
   psk => '${EYP_MCOLLECTIVE_PSK-UgbmV3IG1haWwgaW4gL3Zhci9z}',
   hostname => 'activemq',
-  agent=>true,
-  client=>false,
+  agent=>false,
+  client=>true,
 }
 EOF
 
